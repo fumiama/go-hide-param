0534143024b31d5d2cd544a3989dd2c8baa5d5be
fumiama <s862105088@gmail.com>
2021-09-19 21:45:18 +0800

    fix unix

--- unix.go
+++ unix.go
@@ -12,7 +12,7 @@ func Hide(position int) {
 	if position > 0 && position < len(os.Args) {
 		pwdstr := (*[2]uintptr)(unsafe.Pointer(&os.Args[position]))
 		for i := 0; i < len(os.Args[position]); i++ {
-			*(*uint8)(unsafe.Pointer(uintptr(unsafe.Pointer(pwdstr)) + uintptr(i))) = '*'
+			*(*uint8)(unsafe.Pointer((*pwdstr)[0] + uintptr(i))) = '*'
 		}
 	}
 }
